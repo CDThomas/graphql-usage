40fb68212a3a7c881722469ef467f9319f6e683f
Drew Thomas <cdthomas.92@gmail.com>
2019-06-14 15:31:47 +1000

    Add more tests for findGraphQLTags

--- src/findGraphQLTags.test.ts
+++ src/findGraphQLTags.test.ts
@@ -1,22 +1,86 @@
 import findGraphQLTags from "./findGraphQLTags";
 
-test("returns content of GraphQL tags given JS source code", () => {
-  const js = `
-    import { graphql } from 'react-relay';
-    const query = graphql\`query findGraphQLTagsQuery { hero { id } }\`
-  `;
+describe("findGraphQLTags", () => {
+  test("returns GraphQL tags given JS source code", () => {
+    const js = `
+      import { graphql } from 'react-relay';
+      const query = graphql\`query findGraphQLTagsQuery { hero { id } }\`
+    `;
 
-  expect(findGraphQLTags(js)).toEqual([
-    {
-      sourceLocationOffset: {
-        column: 27,
-        line: 3
+    expect(findGraphQLTags(js)).toEqual([
+      {
+        sourceLocationOffset: {
+          column: 29,
+          line: 3
+        },
+        template: "query findGraphQLTagsQuery { hero { id } }"
+      }
+    ]);
+  });
+
+  test("returns GraphQL tags containing fragments", () => {
+    const js = `
+    import {createFragmentContainer, graphql} from 'react-relay';
+    import TodoItem from './TodoItem'
+
+    export default createFragmentContainer(TodoItem, {
+      hero: graphql\`fragment Hero_hero on Hero { id }\`,
+    });
+    `;
+
+    expect(findGraphQLTags(js)).toEqual([
+      {
+        sourceLocationOffset: {
+          column: 21,
+          line: 6
+        },
+        template: "fragment Hero_hero on Hero { id }"
+      }
+    ]);
+  });
+
+  test("returns GraphQL tags containing mutations", () => {
+    const js = `
+    import {commitMutation, graphql} from 'react-relay';
+
+    const mutation =
+      graphql\`mutation TestMutation($input: ReviewInput!) { createReview(review: $input) { commentary } }\`;
+    `;
+
+    expect(findGraphQLTags(js)).toEqual([
+      {
+        sourceLocationOffset: {
+          column: 15,
+          line: 5
+        },
+        template:
+          "mutation TestMutation($input: ReviewInput!) { createReview(review: $input) { commentary } }"
+      }
+    ]);
+  });
+
+  test("returns GraphQL tags given source code with multiple tags", () => {
+    const js = `
+      import { graphql } from 'react-relay';
+      const queryOne = graphql\`query firstQuery { hero { id } }\`
+      const queryTwo = graphql\`query secondQuery { hero { name } }\`
+    `;
+
+    expect(findGraphQLTags(js)).toEqual([
+      {
+        sourceLocationOffset: {
+          column: 32,
+          line: 3
+        },
+        template: "query firstQuery { hero { id } }"
       },
-      template: "query findGraphQLTagsQuery { hero { id } }"
-    }
-  ]);
+      {
+        sourceLocationOffset: {
+          column: 32,
+          line: 4
+        },
+        template: "query secondQuery { hero { name } }"
+      }
+    ]);
+  });
 });
-
-// TODO: test fragments
-// TODO: test mutations
-// TODO: test multiple tags in one file
