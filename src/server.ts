fe5e6a569b762e6590ea280006c2e8ffeb7f047f
Drew Thomas <cdthomas.92@gmail.com>
2019-07-15 08:30:05 +1000

    Update Express server to serve static files

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { Report } from "./report";
+import path from "path";
 
 function createServer(stats: Report) {
   const app = express();
+
+  app.use(express.static(path.resolve(__dirname, "../graphql-stats-ui/build")));
 
   app.get("/stats", (req, res) => {
     res.json(stats);
