4fdc9a80db2bf96feeef0e019aeb72a487119a63
Drew Thomas <cdthomas.92@gmail.com>
2019-07-30 09:31:52 +1000

    Refactor and improve error handling

--- src/gitUtils.ts
+++ src/gitUtils.ts
@@ -3,33 +3,27 @@ import path from "path";
 import { promisify } from "util";
 
 async function getGitHubBaseURL(sourceDir: string): Promise<string> {
-  // TODO: improve error handling
-  // TODO: check it branch exists in remote and default link to master if not
-
-  const branchOutput = await promisify(exec)(
+  const branchName = await execCommand(
     "git rev-parse --abbrev-ref HEAD",
-    {
-      cwd: path.resolve(sourceDir)
-    }
+    sourceDir
   );
-  const branchName = branchOutput.stdout.toString().trim();
 
   if (!branchName) {
-    throw new Error("Error getting current branch name");
+    throw new Error("Error getting current Git branch name");
   }
 
-  const remoteURLOutput = await promisify(exec)(
+  const remoteURL = await execCommand(
     "git config --get remote.origin.url",
-    { cwd: path.resolve(sourceDir) }
+    sourceDir
   );
   const repoBasePathRegEx = /^git@github\.com:(.*)\.git$/;
-  const matches = remoteURLOutput.stdout
-    .toString()
-    .trim()
-    .match(repoBasePathRegEx);
+  const matches = remoteURL.match(repoBasePathRegEx);
 
   if (!matches || !matches[1]) {
-    throw new Error("Error getting remote URL");
+    throw new Error(
+      "Error getting remote URL. GraphQL Usage requires SOURCE_DIR to be in a Git repository " +
+        "that has been pushed to GitHub."
+    );
   }
 
   const repoBasePath = matches[1];
@@ -38,18 +32,37 @@ async function getGitHubBaseURL(sourceDir: string): Promise<string> {
 }
 
 async function getGitProjectRoot(sourceDir: string): Promise<string> {
-  const projectRootOutput = await promisify(exec)(
-    "git rev-parse --show-toplevel",
-    { cwd: path.resolve(sourceDir) }
-  );
-
-  const projectRoot = projectRootOutput.stdout.toString().trim();
+  const projectRoot = execCommand("git rev-parse --show-toplevel", sourceDir);
 
   if (!projectRoot) {
     throw new Error("Error getting Git project root");
   }
 
   return projectRoot;
+}
+
+function execCommand(command: string, sourceDir: string): Promise<string> {
+  const message =
+    "GraphQL Usage requires git to be available in your PATH " +
+    "and for SOURCE_DIR to be in a Git repository that has been pushed to GitHub.";
+
+  return promisify(exec)(command, { cwd: path.resolve(sourceDir) })
+    .then(({ stdout }) => {
+      return stdout.trim();
+    })
+    .catch(error => {
+      if (error.code === 127) {
+        throw new Error(`Command not found: git. ${message}`);
+      }
+
+      if (error.code === 128) {
+        throw new Error(
+          `SOURCE_DIRECTORY is not in a Git repository or Git has encountered a fatal error. ${message}`
+        );
+      }
+
+      throw error;
+    });
 }
 
 export { getGitHubBaseURL, getGitProjectRoot };
