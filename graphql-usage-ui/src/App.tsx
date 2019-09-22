7769a4ce638ab194a6b50564a8515fbdd095cb6e
Drew Thomas <cdthomas.92@gmail.com>
2019-09-22 11:59:02 +1000

    refactor(ui): extract FieldLine into its own file

--- graphql-usage-ui/src/App.tsx
+++ graphql-usage-ui/src/App.tsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 
 import Delimiter from "./Delimiter";
 import DetailsPanel from "./DetailsPanel";
-import FieldName from "./FieldName";
-import FieldType from "./FieldType";
+import FieldLine from "./FieldLine";
 import Header from "./Header";
 import { Report, ReportField, ReportType } from "./reportTypes";
 import TypeKind from "./TypeKind";
@@ -34,53 +33,6 @@ function Schema({ types, filter, onFieldClick }: SchemaProps) {
           />
         );
       })}
-    </div>
-  );
-}
-
-interface FieldLineProps {
-  field: ReportField;
-  filter: string;
-  onFieldClick(field: ReportField): void;
-}
-function FieldLine({ field, filter, onFieldClick }: FieldLineProps) {
-  const { name, type, occurrences } = field;
-  const lowerCaseFilter = filter.toLowerCase();
-  const fieldMatchesFilter = field.name.toLowerCase().includes(lowerCaseFilter);
-  const typeMatchesFilter = field.type.toLowerCase().includes(lowerCaseFilter);
-  const handleFieldClick = () => onFieldClick(field);
-
-  return (
-    <div
-      style={{ display: "flex", alignItems: "center", paddingLeft: "16px" }}
-      onClick={handleFieldClick}
-    >
-      <div className="field-line" style={{ padding: "10px 0px" }}>
-        <FieldName highlight={!!filter && fieldMatchesFilter}>{name}</FieldName>
-        <Delimiter token={":"} />{" "}
-        <FieldType highlight={!!filter && typeMatchesFilter}>{type}</FieldType>
-      </div>
-
-      {occurrences.length === 0 && (
-        <span
-          style={{
-            display: "inline-block",
-            padding: ".25em .4em",
-            fontSize: "75%",
-            fontWeight: 700,
-            lineHeight: 1,
-            textAlign: "center",
-            whiteSpace: "nowrap",
-            verticalAlign: "baseline",
-            borderRadius: ".25rem",
-            backgroundColor: "#f25c54",
-            color: "#fff",
-            marginLeft: "8px"
-          }}
-        >
-          Unused
-        </span>
-      )}
     </div>
   );
 }
